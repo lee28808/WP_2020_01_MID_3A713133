9d87c73d9153b87018d76f3818b56e569d692d5c
a0953710900@gmail.com <Until Forever@UNTILFOREVER-PC>
2020-11-12 21:46:24 +0800

    加入專案檔案。

---
+++ WP_2020_01_MID_3A713133/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WP_2020_01_MID_3A713133
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.tex = new System.Windows.Forms.TextBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn.Location = new System.Drawing.Point(318, 31);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(129, 55);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "確認";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tex
+            // 
+            this.tex.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tex.Location = new System.Drawing.Point(22, 31);
+            this.tex.Multiline = true;
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(58, 55);
+            this.tex.TabIndex = 1;
+            // 
+            // pic
+            // 
+            this.pic.Image = global::WP_2020_01_MID_3A713133.Properties.Resources._0;
+            this.pic.Location = new System.Drawing.Point(22, 92);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(425, 491);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 595);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox tex;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ WP_2020_01_MID_3A713133/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_MID_3A713133.Properties;
+
+namespace WP_2020_01_MID_3A713133
+{
+    public partial class Form1 : Form
+    {
+        List<Image> pc = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pc.Add(Resources._1);
+            pc.Add(Resources._2);
+            pc.Add(Resources._3);
+            pc.Add(Resources._4);
+            pc.Add(Resources._5);
+            pc.Add(Resources._6);
+            pc.Add(Resources._7);
+            pc.Add(Resources._8);
+            pc.Add(Resources._9);
+            pc.Add(Resources._10);
+            pc.Add(Resources._11);
+            pc.Add(Resources._12);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tex.Text))
+            {
+                MessageBox.Show("請輸入生肖", "錯誤警告");
+                return;//重新檢查
+            }
+            else
+            {
+                switch(tex.Text)
+                {
+                    case "鼠":
+                        pic.Image = pc[0];
+                        break;
+                    case "牛":
+                        pic.Image = pc[1];
+                        break;
+                    case "虎":
+                        pic.Image = pc[2];
+                        break;
+                    case "兔":
+                        pic.Image = pc[3];
+                        break;
+                    case "龍":
+                        pic.Image = pc[4];
+                        break;
+                    case "蛇":
+                        pic.Image = pc[5];
+                        break;
+                    case "馬":
+                        pic.Image = pc[6];
+                        break;
+                    case "羊":
+                        pic.Image = pc[7];
+                        break;
+                    case "猴":
+                        pic.Image = pc[8];
+                        break;
+                    case "雞":
+                        pic.Image = pc[9];
+                        break;
+                    case "狗":
+                        pic.Image = pc[10];
+                        break;
+                    case "豬":
+                        pic.Image = pc[11];
+                        break;
+                    default:
+                        MessageBox.Show("請重新輸入生肖", "錯誤警告");
+                        break;
+                }
+                return;
+            }
+        }
+    }
+}
